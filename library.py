97662ca4438ca48b348a3493c95f34e699fb0349
raphaelzhou1 <77868072+raphaelzhou1@users.noreply.github.com>
2022-08-03 22:21:10 -0400

    Update library.py

--- library.py
+++ library.py
@@ -1,2 +1,20 @@
+# all the useful Python libraries discovered
+
+# MOUSE libraries
+
+# To get Python basic interactions
+# https://pypi.org/project/mouse/
+import mouse
+
+# To use Python to move mouse
+# https://www.geeksforgeeks.org/mouse-keyboard-automation-using-python/
+import pyautogui
+________________________________________________________________________________________________________________
+
+# KEYBOARD libraries
+
+#
 import cv2
+
+# extract Pdf data
 from invoice2data import extract_data
